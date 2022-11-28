55fde1450dd6d1dc2b355fd196c67282be6b97bb
nerots123 <hamdanubaidillah0306@gmail.com>
2022-11-28 19:01:57 +0700

    add padding

--- src/components/project.jsx
+++ src/components/project.jsx
@@ -2,7 +2,7 @@ import Cards from "./utilities/card";
 
 const Project = () => {
   return (
-    <main id="project" className="bg-zinc-800 text-white h-screen max-sm:h-auto pt-24 mb-6">
+    <main id="project" className="bg-zinc-800 text-white h-screen max-sm:h-auto pt-24 pb-6">
       <div className="w-full">
         <h1 className="text-center text-4xl max-md:text-xl  w-max m-auto px-5 font-bold pb-2 ">Projects</h1>
       </div>
